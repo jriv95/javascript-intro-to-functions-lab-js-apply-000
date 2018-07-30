4ea7e1171bfef2f2f53bd3a4a2fce77d6923276e
Joel Rivera <joel.rivera7995@gmail.com>
2018-07-30 21:47:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,6 @@ function logWhisper(string){
   console.log(string.toLowerCase())
 }
 
-function sayHiToGrandma(string){
+
   if(string === lowercase)
-  return "I can'\t hear you!"
-}
+  return "I can'\t hear you!";
